77bebda907b932460d72f2db6121d801e5631b09
eliran <eliranovadia7@gmail.com>
2026-04-27 23:47:34 +0300

    starting over

--- controllers/dashboard_controller.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from UI.Dashboard_ui import Ui_dashboard_ui
-
-class DashboardApp(QMainWindow):
-    def __init__(self, main_controller):
-        super().__init__()
-        self.ui = Ui_dashboard_ui()
-        self.ui.setupUi(self)
-        self.main_controller = main_controller
--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from PyQt5.QtWidgets import QApplication
-from controllers.main_controller import MainController
-
-def main():
-    app = QApplication([])
-    controller = MainController()
-    controller.show_login_window()
-    app.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
+print("Starting over")
